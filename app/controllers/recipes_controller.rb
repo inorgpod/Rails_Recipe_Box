4c08692769d83ade5b1b943f2041c56e09eb6961
Gao Ciaorun <itchynozz@gmail.com>
2020-07-13 04:15:35 +0800

    simple form added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,37 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe,  only: [:show, :edit, :update, :destroy]
   def index 
+    @recipe = Recipe.all
   end
+
+  def show 
+    @recipe = Recipe.find.params([:id])
+  end
+  
+  def new 
+    @recipe = Recipe.new
+  end
+
+  def create 
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save 
+      redirect_to @recipe, notice: 'Successfully created new recipe' 
+    else 
+      render 'new' 
+    end
+  end
+
+
+
+  private 
+
+  def recipe_params 
+    params.require(:recipe).permit(:title, :description)
+  end
+
+  def find_recipe 
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
